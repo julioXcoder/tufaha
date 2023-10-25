0e6e83c7bc6583c8061fde674c13ab3d113c211c
Julio Oran Njeza <julioxcoder@gmail.com>
2023-10-26 00:07:37 +0300

    Setup Roboto font

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
+        consequuntur neque possimus obcaecati explicabo voluptas aspernatur?
+        Quae, fuga. Recusandae unde ad amet! Illum repudiandae quasi
+        reprehenderit, impedit rerum pariatur debitis unde quisquam dolor cumque
+        molestias atque sapiente amet dignissimos at quis a nostrum dolorum non
+        ipsa! Nemo dolore, dicta temporibus quos voluptatibus dignissimos
+        impedit perferendis doloribus quisquam numquam, quo iusto facere
+        corrupti nihil consequuntur iure repudiandae nesciunt at sit eos ratione
+        esse id provident omnis. Quisquam molestiae esse, quo reiciendis
+        dignissimos tenetur eos voluptates, velit, aliquam omnis eum debitis aut
+        vero adipisci dolor. Fuga sequi ex odit dolore! Culpa, voluptate!
+      </p>
+    </div>
+  );
 }
